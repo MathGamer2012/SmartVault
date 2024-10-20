51c7a1334f0dc8cf34461665752c7796bfd665da
Ishaan Mittal <123089344+MathGamer2012@users.noreply.github.com>
2024-10-19 23:44:37 -0400

    Add files via upload

---
+++ src/Swing/noteboard/ModelNoteBoard.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Swing.noteboard;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Danial Suhail
+ */
+public class ModelNoteBoard {
+
+    /**
+     * @return the titleColor
+     */
+    public Color getTitleColor() {
+        return titleColor;
+    }
+
+    /**
+     * @param titleColor the titleColor to set
+     */
+    public void setTitleColor(Color titleColor) {
+        this.titleColor = titleColor;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the time
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public ModelNoteBoard(Color titleColor, String title, String time, String description) {
+        this.titleColor = titleColor;
+        this.title = title;
+        this.time = time;
+        this.description = description;
+    }
+    
+    public ModelNoteBoard() {
+        
+    }
+    private Color titleColor;
+    private String title;
+    private String time;
+    private String description;
+}
